cad847cf88f0e776bd53dbfe6c0c7db4f47d25b4
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-21 22:16:35 +0530

    java tut + source code

---
+++ Learning Java/intro.java
@@ -0,0 +1,22 @@
+
+/**
+ * intro
+ */
+public class intro {
+
+    public static void main(String[] args) {
+        System.out.println("Big F***ing Rocket-Elon Musk");
+        String name = "Dhara";
+        int p = 466, m = 645;
+        float d = 78.524f;
+        boolean ai = false;
+        double db = 78.236589d;
+        char grade = 'O';
+        System.out.println(name);
+        System.out.println(p);
+        System.out.println(d);
+        System.out.println(ai);
+        System.out.println(db);
+        System.out.println(grade);
+    }
+}
