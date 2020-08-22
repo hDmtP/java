84ab302191e1f3d07cce63ae0e80306a2631bd34
Pritam Dhara <pritamdhara914@gmail.com>
2020-08-22 19:14:54 +0530

    upto maths

---
+++ Learning Java/hjbh.java
@@ -0,0 +1,9 @@
+/**
+ * hjbh
+ */
+public class hjbh {
+
+    public static void main(String[] args) {
+        System.out.println("Was just looking how to initiate a java program");
+    }
+}
--- Learning Java/intro.java
+++ Learning Java/intro.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /*
  * intro
@@ -5,6 +6,7 @@
 public class intro {
 
     public static void main(String[] args) {
+       /*
         System.out.println("Big F***ing Rocket-Elon Musk");
         String name = "Dhara";
         int p = 466, m = 645;
@@ -18,5 +20,84 @@ public class intro {
         System.out.println(ai);
         System.out.println(db);
         System.out.println(grade);
+        */
+
+        /* Operators in Java
+        operand operator operand  =  result
+           1       +       2      =  result
+
+        Tyeps of operators in Java:
+        1.Arithmatic
+        2.Assignment
+        3.Logical
+        4.Comparison   
+        */
+        /*Arithmatic operators*/ 
+        // int num1=789, num2=456;
+        // num1+=123;
+        // num1-=123;
+        // num2*=12;
+        // num2/=3;
+        // num2%=3;
+        // System.out.println(num1+num2);
+        // System.out.println(num1-num2);
+        // System.out.println(num1*num2);
+        // System.out.println(num1/num2);
+        // System.out.println(num1%num2);
+        // System.out.println(num2++);
+        // System.out.println(++num2);
+        // System.out.println(num2--);
+        // System.out.println(--num2);
+
+        /*Comparison operators 
+        1. ==
+        2. !=
+        3. >
+        4.<
+        */
+
+        /*Logical operators
+        1. &&
+        2. || --> (or) 
+        3. !
+        */
+
+        /*Taking user input in Java */
+      /* Scanner scan = new Scanner(System.in);
+       System.out.println("Enter an user input:\n");
+    //    String input = scan.nextLine();
+       int input = scan.nextInt();
+       System.out.print(input);
+       */
+
+       /*String method */
+    //    String kasukabe = "Kasama";
+    //    System.out.println(kasukabe);
+    //    System.out.println(kasukabe+"\"Nani\"");
+    //    System.out.println(kasukabe.length());
+    //    System.out.println(kasukabe.toUpperCase());
+    //    System.out.println(kasukabe.toLowerCase());
+    //    System.out.println(kasukabe.contains("sama"));
+    //    System.out.println(kasukabe.contains("os"));
+    //    System.out.println(kasukabe.charAt(3));
+    //    System.out.println(kasukabe.endsWith("ma"));
+    //    System.out.println(kasukabe.indexOf("xy"));
+    //    System.out.println(kasukabe.indexOf("Ka"));
+
+    /*Math class in Java */
+
+    int a=458, b=1478;
+    System.out.println(Math.max(a, b));
+    System.out.println(Math.min(a, b));
+    System.out.println(Math.sqrt(1024));
+    System.out.println(Math.abs(-24));
+    System.out.println(Math.abs(10));
+    System.out.println(Math.random()); //--->will print any random number between 0 and 1
+
+
+
+        
+
+
     }
 }
